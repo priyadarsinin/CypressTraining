27690e9a525da22f732a1c1eae5b2fc3cf265607
priyadarsinin <priyadarsini@qaoncloud.com>
2024-11-23 16:40:00 +0530

    Update Checkbox.cy.js

--- Checkbox.cy.js
+++ Checkbox.cy.js
@@ -4,7 +4,7 @@ describe("Check Boxes",function(){
 
         cy.visit("https://demo.guru99.com/test/radio.html")
 
-        // Selecting Single Checkboxes
+       
 
         cy.get("input#vfb-6-0").check().should("be.checked")
         cy.get("input#vfb-6-1").check().should("be.checked")
@@ -16,25 +16,25 @@ describe("Check Boxes",function(){
 
     it("Multiple Selection",function(){
 
-        // Multiple Selection
+        
 
         cy.visit("https://demo.guru99.com/test/radio.html")
         cy.get('input[type="checkbox"]').check().should("be.checked")
     
-        // Uncheck all the checkboxes
+        
         cy.get('input[type="checkbox"]').uncheck().should("not.be.checked")
 
     })
 
     it("Checking First and Last checkbox",function(){
 
-        // Checking First and Last checkbox
+        
 
         cy.visit("https://demo.guru99.com/test/radio.html")
         cy.get('input[type="checkbox"]').check().first().should("be.checked")
         cy.get('input[type="checkbox"]').check().last().should("be.checked")
     
-        // Uncheck first checkboxes
+        
         cy.get('input[type="checkbox"]').first().uncheck().should("not.be.checked")
 
     })
