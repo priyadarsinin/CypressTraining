1b98cdd5db5bfe7bc4439a8bec18f6dc1cc68c27
priyadarsinin <priyadarsini@qaoncloud.com>
2024-12-12 22:19:31 +0530

    Add files via upload

---
+++ http2.cy.js
@@ -0,0 +1,22 @@
+describe("HTTP Requests", () => {  
+    it("GET Call", () => {  
+   cy.request('GET', 'https://jsonplaceholder.typicode.com/posts/1')  
+   .its('status')  
+   .should('equal', 200);  
+    })  
+    
+    it("Post Call", () => {  
+   cy.request({  
+   method: 'POST',  
+   url: 'https://jsonplaceholder.typicode.com/posts/',  
+   body: {  
+    title: "Test post",  
+    body: "This is post call",  
+   userId: 1  
+   }  
+    })  
+   .its('status')  
+   .should('equal', 201);  
+    })  
+  })
+  
